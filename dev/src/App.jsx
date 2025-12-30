f4f1bdf29a13feefe0d34ddf1f93194c56dc38e3
ganesh sirasani <ganeshsirasani@gmail.com>
2025-12-30 18:05:17 +0530

    nav

--- dev/src/App.jsx
+++ dev/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Reagester from './Reagester'
 
 function App() {
   
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
        <h1>hi</h1>
+       <Reagester/>
     </>
   )
 }
---
+++ dev/src/Reagester.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Reagester() {
+  return (
+    <div>Reagester</div>
+  )
+}
+
+export default Reagester
